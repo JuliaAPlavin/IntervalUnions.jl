4933217e4f39ef267b744425f0df9501677fc255
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    actual implementation

--- src/IntervalUnions.jl
+++ src/IntervalUnions.jl
@@ -1,3 +1,50 @@
 module IntervalUnions
 
+using IntervalSets
+using Accessors
+using DataPipes
+
+export IntervalUnion
+
+struct IntervalUnion{T} <: IntervalSets.Domain{T}
+    ints::T
+end
+
+intervals(x::Interval) = (x,)
+@accessor intervals(x::IntervalUnion) = x.ints
+
+Base.:(==)(a::IntervalUnion, b::IntervalUnion) = intervals(a)::Tuple == intervals(b)::Tuple
+Base.:(==)(a::IntervalUnion, b::Interval) = intervals(a)::Tuple == intervals(b)::Tuple
+Base.:(==)(a::Interval, b::IntervalUnion) = intervals(a)::Tuple == intervals(b)::Tuple
+
+IntervalUnion(x::Union{Interval, IntervalUnion}) = convert(IntervalUnion, x)
+Base.convert(::Type{IntervalUnion}, x::IntervalUnion) = x
+Base.convert(::Type{IntervalUnion}, x::Interval) = IntervalUnion((x,))
+
+Base.in(x, iu::IntervalUnion) = any(in(x, i) for i in intervals(iu))
+Base.isempty(iu::IntervalUnion) = all(isempty, intervals(iu))
+_dropempty(iu::IntervalUnion) = IntervalUnion(filter(!isempty, intervals(iu)))
+
+_opposite_closedness(x::Symbol) = x == :closed ? :open : x == :open ? :closed : error()
+_setdiff(a::Interval{La, Ra}, b::Interval{Lb, Rb}) where {La, Ra, Lb, Rb} = intersect(a, complement(b))
+
+complement(i::Interval{L,R,T}) where {T,L,R} = IntervalUnion((
+    Interval{:open, _opposite_closedness(L)}(-T(Inf), leftendpoint(i)),
+    Interval{_opposite_closedness(R), :open}(rightendpoint(i), T(Inf)),
+))
+
+Base.setdiff(a::Interval, b::IntervalUnion) = @p intervals(b) |> map(_setdiff(a, _)) |> reduce(∩) |> _dropempty
+Base.setdiff(a::IntervalUnion, b::Union{Interval, IntervalUnion}) = @p intervals(a) |> map(setdiff(_, IntervalUnion(b))) |> reduce(∪)
+Base.:∪(a::Interval, b::IntervalUnion) = IntervalUnion((intervals(a)..., intervals(b)...))
+Base.:∪(a::IntervalUnion, b::Union{Interval, IntervalUnion}) = IntervalUnion((intervals(a)..., intervals(b)...))
+Base.:∩(a::Interval, b::IntervalUnion) = @modify(i -> a ∩ i, intervals(b) |> Elements()) |> _dropempty
+Base.:∩(a::IntervalUnion, b::Interval) = @modify(i -> b ∩ i, intervals(a) |> Elements()) |> _dropempty
+Base.:∩(a::IntervalUnion, b::IntervalUnion) = @p intervals(a) |> map(_ ∩ b) |> reduce(∪)
+
+IntervalSets.infimum(iu::IntervalUnion) = minimum(infinimum, intervals(iu))
+IntervalSets.supremum(iu::IntervalUnion) = maximum(supremum, intervals(iu))
+Base.minimum(iu::IntervalUnion) = minimum(minimum, intervals(iu))
+Base.maximum(iu::IntervalUnion) = maximum(maximum, intervals(iu))
+Base.extrema(iu::IntervalUnion) = (minimum(iu), maximum(iu))
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,48 @@ using TestItemRunner
 @run_package_tests
 
 
+@testitem "basic usage" begin
+    using IntervalSets
+    using IntervalUnions: intervals
+
+    x = 1..2.
+    a = IntervalUnion(x)
+    @test length(intervals(a)) == 1
+    @test intervals(a)[1] === x
+
+    x2 = 3..4.
+    b = IntervalUnion([x, x2])
+    @test length(intervals(b)) == 2
+    @test intervals(b) == [x, x2]
+
+    b = IntervalUnion((x, x2))
+    @test length(intervals(b)) == 2
+    @test intervals(b) === (x, x2)
+
+    x3 = 5..6.
+    b3 = b ∪ x3
+    @test intervals(b3) === (x, x2, x3)
+
+    @test intervals(setdiff(b3, x2)) === (x, x3)
+    @test intervals(setdiff(b3, 3.3..3.5)) === (1..2., Interval{:closed, :open}(3, 3.3), Interval{:open, :closed}(3.5, 4), 5..6.)
+    @test intervals(setdiff(b3, 3.3..10)) === (1..2., Interval{:closed, :open}(3, 3.3))
+    @test intervals(setdiff(b3, IntervalUnion((3.3..3.5, 5..10.)))) === (1..2., Interval{:closed, :open}(3, 3.3), Interval{:open, :closed}(3.5, 4))
+    @test intervals(b3 ∩ IntervalUnion((3.3..3.5, 5..10.))) === (3.3..3.5, 5..6.)
+
+    c = x ∪ (1..0)
+    @test length(intervals(c)) == 1
+    @test intervals(c)[1] == x
+
+    @test IntervalUnion(1..2) == 1..2
+    @test isempty(IntervalUnion(1..0))
+    @test !isempty(IntervalUnion(1..2))
+
+    @test 3.3 ∈ b
+    @test 4.3 ∉ b
+    @test extrema(b) == (1, 4)
+end
+
+
 @testitem "_" begin
     import Aqua
     Aqua.test_all(IntervalUnions; ambiguities=false)
